72aceb3e5aa1e779462d1a0cd5a1e80ec72bf23b
naru-19 <narururu19@gmail.com>
2023-02-04 17:58:21 +0900

    update test code

--- tests/test_digit_number.py
+++ tests/test_digit_number.py
@@ -1,8 +1,7 @@
-from digital_number import num2img
+from imgarr.digital_number import num2img
 
 
 def test_num2img(capsys):
     num2img(5)
     num2img(5,fix_digit=2)
     num2img(5,fix_digit=2,size=(15,15))
-    
